dd303c4ba7a86877240441c87be385c5bb0ae766
mite404 <ethana2008@gmail.com>
2024-04-15 10:17:12 -0400

    starting 100 days of python.

---
+++ bandname_gen.py
@@ -0,0 +1,11 @@
+print("Welcome\n"
+      "to the\n"
+      "+++++ Band Name Generator +++++")
+city = input("What city did you grow up in?").upper()
+pet = input("What was the name of your favorite pet?").upper()
+street = input("What street did you grow up on?").upper()
+print("...")
+print("...")
+print("...generating...")
+print(" +++++|*~   YOUR BAND NAME IS   ~*|+++++")
+print(f"The {city} {pet} {street}s ")
---
+++ ceasar_cypher/ceasar_cypher.py
@@ -0,0 +1,15 @@
+# def greet():
+#     print(f"Hi {name}!")
+#     name = input("What's yr name, slim?")
+# greet()
+
+
+
+
+
+def greet_with_name(name):
+    print(f"Hi {name}!")
+    print(f"How do you do {name}?")
+    print("Isn't the weather nice?")
+
+greet_with_name("dan")
