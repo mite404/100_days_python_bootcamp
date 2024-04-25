d125650c183939c99042c47b6c0ac6d9cd6f0dd0
mite404 <ethana2008@gmail.com>
2024-04-24 23:38:29 -0400

    figuring out how to use PrettyTable.

---
+++ 16_OOP/pretty_table.py
@@ -0,0 +1,9 @@
+from prettytable import PrettyTable
+table = PrettyTable()
+table.field_names = ["City Name", "Arena", "Population", "Annual Rainfall"]
+table.add_row(["Adelaide", 1295, 1158259, 600.5])
+table.add_row(["Brisbane", 5905, 1857594, 1146.4])
+table.add_row(["Darwin", 112, 120900, 1714.7])
+table.add_row(["Hobart", 1357, 205556, 619.5])
+table.align = "c"
+print(table)
